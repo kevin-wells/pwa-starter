1dfdbbe2341f73fec3928760b2f3304d57c23f09
kevin-wells <kevin-wells@users.noreply.github.com>
2022-12-23 03:04:21 +0000

    deploy: eeafca000a5fc1ea77db70c362893f312f014104

--- code/app-about.98e58701.js
+++ code/app-about.98e58701.js
@@ -1,4 +1,4 @@
-import{i as c,s as h,y as u,a as i,e as d}from"./index.49c8c68d.js";const l=c`
+import{i as c,s as h,y as u,a as i,e as d}from"./index.fb80a2b4.js";const l=c`
   @media(min-width: 1000px) {
     sl-card {
       max-width: 70vw;
@@ -24,4 +24,4 @@ import{i as c,s as h,y as u,a as i,e as d}from"./index.49c8c68d.js";const l=c`
         </sl-card>
   </main>
     `}};p.styles=[i,l];p=b([d("app-about")],p);export{p as AppAbout};
-//# sourceMappingURL=app-about.c0a3c392.js.map
+//# sourceMappingURL=app-about.98e58701.js.map
